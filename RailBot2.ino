ab7fbbb96b7a35d5052d3733bbe5005d823f4da0
aarodav1 <aarodav1@umbc.edu>
2015-04-03 20:50:28 -0400

    Update
    Cleaned up and added comments to file, deleted some unnecessary stuff.
    Also uploaded the RF24 library which you need to download and add to
    your sketch to be able to compile it

--- RailBot2.ino
+++ RailBot2.ino
@@ -1,10 +1,4 @@
-
-/* YourDuinoStarter Example: nRF24L01 Receive Joystick values
-
- - WHAT IT DOES: Receives data from another transceiver with
-   2 Analog values from a Joystick or 2 Potentiometers
-   Displays received values on Serial Monitor
- - SEE the comments after "//" on each line below
+/*
  - CONNECTIONS: nRF24L01 Modules See:
  http://arduino-info.wikispaces.com/Nrf24L01-2.4GHz-HowTo
    1 - GND
@@ -22,35 +16,97 @@
 
 /*-----( Import needed libraries )-----*/
 #include <SPI.h>
-#include "nRF24L01.h"
+//#include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
 #include <SD.h>
 
+//Pin definitions
+//Digital
 #define SD_CS 24
 #define RF_CS 22
 #define RF_CSN 23
-
 #define MOTOR_1 25
 #define MOTOR_2 26
+
+//Analog
 #define MOTOR_EN 15
+
+//Other definitions
 #define MAX_SPEED 255
 
-//NOTE: the "LL" at the end of the constant is "LongLong" type
+//Global Variables
 const uint64_t pipe = 0xF0F0F0F0D2LL; // Define the transmit pipe
 long count;
 long countIncrement;
-
-/*-----( Declare objects )-----*/
 RF24 radio(RF_CS,RF_CSN); // Create a Radio
 
-//play with stop condition
+
+
+
+  
+ //Set up all required I/O and global variables
+void setup()   /****** SETUP: RUNS ONCE ******/
+  {
+    //115200 baud for laser
+    Serial.begin(115200);
+  
+  
+    //Initialize the SD card
+    Serial.println("Initializing SD card...");
+  
+    if(SD.begin(SD_CS)){
+       Serial.println("SD card initialized successfully.\n");
+    }
+  
+    //Set up rotary encoder interrupt
+    count = 0;
+    attachInterrupt(2, countInt, CHANGE);
+  
+    //Set up motor pins
+    pinMode(MOTOR_1, OUTPUT);
+    pinMode(MOTOR_2, OUTPUT);
+    pinMode(MOTOR_EN, OUTPUT);
+ 
+    digitalWrite(MOTOR_1, HIGH);
+    digitalWrite(MOTOR_2, LOW);
+    analogWrite(MOTOR_EN, 0);
+  
+    //RF Initialization
+    printf_begin();
+    radio.begin();
+    Serial.println("RF Module information:");
+    radio.printDetails();
+  
+  }//--(end setup )---
+
+
+//The magic occurs here
+void loop()   /****** LOOP: RUNS CONSTANTLY ******/
+{ 
+  
+}//--(end main loop )---
+
+
+//-- USER FUNCTIONS 
+
+//This function moves the robot the distance represented by the value in
+//countIncrement. This variable will be set during the initialization
+//based on the increment distance configured by the user before starting the survey.
+//The idea of this function is to speed up to MAX_SPEED in a controlled fashion
+//instead of instantly going to MAX_SPEED. The motor speed will increase up to a 
+//max of MAX_SPEED until it has traversed half the required distance, and then will
+//begin to slow back down to a stop for the second half.
+
+//NOTE: play with delay values and while loop conditions once motor is functional
 void move () {
   
+  //save the current position
   long currentCount = count;
   
   int motorSpeed = 0;
   
+   //speed up until half of the distance is traversed
    while (count < (currentCount + countIncrement / 2)) {
       
      analogWrite(MOTOR_EN, motorSpeed);
@@ -64,6 +120,7 @@ void move () {
  
      }
 
+     //Speed down in the exact opposite fashion as above
      while (count < (currentCount + countIncrement)) {
       
        analogWrite(MOTOR_EN, motorSpeed);
@@ -78,68 +135,30 @@ void move () {
  
      }
   
+    //Cause the H-bridge to brake the motor
     digitalWrite(MOTOR_1, LOW);
     analogWrite(MOTOR_EN, 255);
     
     delay(50);
     
+    //Set motor back to the forward direction and disable it
     analogWrite(MOTOR_EN, 0);
     digitalWrite(MOTOR_1, HIGH);   
-  }
   
-  return;
+    return;
   
 }  
-  
-  void setup()   /****** SETUP: RUNS ONCE ******/
-{
-  Serial.begin(115200);
-  
-  Serial.println("Initializing SD card...");
-  
-  if(SD.begin(SD_CS)){
-     Serial.println("SD card initialized successfully.\n");
-  }
-  
-  //Set up rotary encoder interrupt
-  count = 0;
-  attachInterrupt(2, countInt, CHANGE);
-  
-  //Set up motor pins
-  pinMode(MOTOR_1, OUTPUT);
-  pinMode(MOTOR_2, OUTPUT);
-  pinMode(MOTOR_EN, OUTPUT);
- 
-  digitalWrite(MOTOR_1, HIGH);
-  digitalWrite(MOTOR_2, LOW);
-  digitalWrite(MOTOR_EN, LOW);
-  
-  //RF Initialization
-  printf_begin();
-  radio.begin();
-  Serial.println("RF Module information:");
-  radio.printDetails();
-  
-}//--(end setup )---
 
-
-void loop()   /****** LOOP: RUNS CONSTANTLY ******/
-{ 
-  
-}//--(end main loop )---
-
-
-
+//ISR for the rotary encoder
+//Increments the counter variable by 1 every time it is triggered on both the rising
+//and falling edge of the signal. This translates into 16 interrupts per rotation of the 
+//disc. With a 1 5/8 diameter wheel, this equals 0.38" per interrupt
 void countInt() {
   count++;
+  
+  //For testing only
   Serial.print("Count = ");
   Serial.println(count);
 }
-
-/*-----( Declare User-written Functions )-----*/
-
-//NONE
-//*********( THE END )***********
-
 
 
